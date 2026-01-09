84a3acdde67ede721717decf8992024e3348ef8e
Sama <112719147+SdotKab@users.noreply.github.com>
2026-01-08 20:34:18 -0500

    Landing Page

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import HeroSection from './HeroSection'
+import FeatureSection from './FeatureSection'
+import DiscoverSection from './DiscoverSection'
+import CallToActionSection from './CallToActionSection'
+import FooterSection from './FooterSection'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <HeroSection />
+      <FeatureSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
+    </div>
   )
 }
 
